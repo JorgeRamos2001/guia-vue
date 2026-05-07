5e1daa3bcd92ddd7da0668d39a637021a1696545
Jorge Ramos <ramosjar2001@gmail.com>
2026-05-06 22:03:38 -0600

    Error de gh pages solucionado

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(), // URLs limpias sin #
+    history: createWebHistory(import.meta.env.BASE_URL), // URLs limpias sin #
     routes
 })
 
